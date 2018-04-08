3b4d76a9e6cbd3fca12e20b484b410f238c6416c
DreamLi <18829346477@163.com>
2018-04-08 14:34:19 +0800

    Initial commit

---
+++ src/com/dreamli/datasource/tomcat/TomcatDS.java
@@ -0,0 +1,57 @@
+package com.dreamli.datasource.tomcat;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+@WebServlet(value="/test", loadOnStartup=1)
+public class TomcatDS extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() throws ServletException {
+		System.out.println("===============================init====");
+		try {
+			Context initCtx = new InitialContext();
+			Context envCtx = (Context) initCtx.lookup("java:comp/env");
+			DataSource ds = (DataSource) envCtx.lookup("jdbc/ds");
+
+			Connection connection = ds.getConnection();
+
+			System.out.println(connection);
+			
+			PreparedStatement statement = connection.prepareStatement("select * from t_user");
+			ResultSet resultSet = statement.executeQuery();
+			
+			while(resultSet.next()) {
+				String name = resultSet.getString("name");
+				
+				System.out.println(name);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
